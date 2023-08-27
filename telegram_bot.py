a40d18d8ed71ace2be21c614a045e5c17b8eea8e
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-08-27 17:02:20 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,5 +2,5 @@ import telegram
 
 
 bot = telegram.Bot(token='6548050139:AAGf7MR4IC0UFTBjvnlmfoDhhRQOthzGudI')
-bot.send_message(chat_id='@NASA_images_2023', text="I'm sorry Dave I'm afraid I can't do that.")
-print(bot.get_me())
+# bot.send_message(chat_id='@NASA_images_2023', text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id='@NASA_images_2023', document=open('NewImages/spacex0.jpg', 'rb'))
