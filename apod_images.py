c626b71dfea81ead7057c3efd0eba68e41309dd8
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-08-27 22:40:42 +0300

    Update README.MD.

--- apod_images.py
+++ apod_images.py
@@ -9,15 +9,19 @@ from urllib.parse import urlsplit
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
+def get_json():
     token_API = os.getenv("API_KEY")
     url = 'https://api.nasa.gov/planetary/apod'
     query_params = {'api_key': token_API, 'count': '30'}
     params = urllib.parse.urlencode(query_params)
     response = requests.get(url, params=params)
-    json_data = json.loads(response.text)
+    return json.loads(response.text)
+
+
+def main():
+    load_dotenv()
     name_folder = download.folder("NewImages")
+    json_data = get_json()
     for jpg_number, jpg_url in enumerate(json_data):
         media_type = jpg_url['media_type']
         if media_type == 'image':
