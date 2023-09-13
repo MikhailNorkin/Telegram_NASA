6f3a6e5e45135fe38b6f0d9df4975058ac297300
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-09-13 14:50:14 +0300

    Changed name API_token

--- apod_images.py
+++ apod_images.py
@@ -7,9 +7,9 @@ from urllib.parse import urlsplit
 from dotenv import load_dotenv
 
 
-def get_nasa_pictures(API_token):
+def get_nasa_pictures(api_token):
     url = 'https://api.nasa.gov/planetary/apod'
-    query_params = {'api_key': API_token, 'count': '30'}
+    query_params = {'api_key': api_token, 'count': '30'}
     response = requests.get(url, params=query_params)
     return response.json()
 
@@ -18,8 +18,8 @@ def main():
     load_dotenv()
     folder_name = "NewImages"
     os.makedirs(folder_name, exist_ok=True)
-    API_token = os.getenv("API_KEY")
-    nasa_pictures = get_nasa_pictures(API_token)
+    api_token = os.getenv("API_KEY")
+    nasa_pictures = get_nasa_pictures(api_token)
     for jpg_number, jpg_url in enumerate(nasa_pictures):
         media_type = jpg_url['media_type']
         if media_type != 'image':
