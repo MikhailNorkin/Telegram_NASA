cbdf6455356d5183e5f04e21a412a275ff12c0e1
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-09-13 23:54:47 +0300

    Update telegram_bot.py and telegram_image.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,22 +7,26 @@ from dotenv import load_dotenv
 
 
 # Задача 19. Публикуйте фотографии автоматически
-load_dotenv()
-token = os.getenv("TOKEN_TELEGRAM")
-bot = telegram.Bot(token=token)
-parser = argparse.ArgumentParser(description='Uploading images to telegram bot @NASA_images_2023')
-parser.add_argument("-s", "--seconds_sleep", type=int, help="Enter seconds to sleep", default=os.getenv("TIME_SLEEP"))
-arg = parser.parse_args()
-seconds_sleep = arg.seconds_sleep
-files_nasa = []
-while True:
-    list_count = len(files_nasa)
-    if list_count > 0:
-        patch_image = """NewImages/{files_nasa}""".format(files_nasa=files_nasa[0])
-        bot.send_document(chat_id='@NASA_images_2023', document=open(patch_image, 'rb'))
-        del files_nasa[0]
-        time.sleep(seconds_sleep)
-    else:
-        for root, dirs, files_nasa in os.walk('NewImages/'):
-            random.shuffle(files_nasa)               
+def main():
+    load_dotenv()
+    token = os.getenv("TOKEN_TELEGRAM")
+    bot = telegram.Bot(token=token)
+    parser = argparse.ArgumentParser(description='Uploading images to telegram bot @NASA_images_2023')
+    parser.add_argument("-s", "--seconds_sleep", type=int, help="Enter seconds to sleep", default=os.getenv("TIME_SLEEP"))
+    arg = parser.parse_args()
+    seconds_sleep = arg.seconds_sleep
+    files_nasa = []
+    while True:
+        list_count = len(files_nasa)
+        if list_count > 0:
+            patch_image = """NewImages/{files_nasa}""".format(files_nasa=files_nasa[0])
+            bot.send_document(chat_id='@NASA_images_2023', document=open(patch_image, 'rb'))
+            del files_nasa[0]
+            time.sleep(seconds_sleep)
+        else:
+            for root, dirs, files_nasa in os.walk('NewImages/'):
+                random.shuffle(files_nasa)               
 
+
+if __name__ == '__main__':
+    main()
--- telegram_image.py
+++ telegram_image.py
@@ -6,17 +6,22 @@ from dotenv import load_dotenv
 
 # Задача 18. Опубликуйте картинку в Telegram-канал.
 # Публикует указанную фотографию в канал. Если “какую” не указано, публикует случайную фотографию.
-load_dotenv()
-token = os.getenv("TOKEN_TELEGRAM")
-bot = telegram.Bot(token=token)
-parser = argparse.ArgumentParser(description='Uploading image to telegram bot @NASA_images_2023')
-parser.add_argument("-n", "--name_image", type=str, help="Enter name image", default="")
-arg = parser.parse_args()
-name_image = arg.name_image
-if name_image == "":
-    for root, dirs, list_files in os.walk('NewImages/'):
-        random.shuffle(list_files)
-        file_path = 'NewImages/' + list_files[0]
-else:
-    file_path = 'NewImages/' + name_image
-bot.send_document(chat_id='@NASA_images_2023', document=open(file_path, 'rb'))
+def main():
+    load_dotenv()
+    token = os.getenv("TOKEN_TELEGRAM")
+    bot = telegram.Bot(token=token)
+    parser = argparse.ArgumentParser(description='Uploading image to telegram bot @NASA_images_2023')
+    parser.add_argument("-n", "--name_image", type=str, help="Enter name image", default="")
+    arg = parser.parse_args()
+    name_image = arg.name_image
+    if name_image == "":
+        for root, dirs, list_files in os.walk('NewImages/'):
+            random.shuffle(list_files)
+            file_path = 'NewImages/' + list_files[0]
+    else:
+        file_path = 'NewImages/' + name_image
+    bot.send_document(chat_id='@NASA_images_2023', document=open(file_path, 'rb'))
+
+
+if __name__ == '__main__':
+    main()
